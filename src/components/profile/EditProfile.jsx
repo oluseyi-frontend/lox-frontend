cd1b2e17e40366b2e37634e10fa81b882e6df202
kay1234567 <alawiyeolukayode@gmail.com>
2021-07-21 12:12:21 +0100

    input

--- src/components/profile/EditProfile.jsx
+++ src/components/profile/EditProfile.jsx
@@ -1,32 +1,30 @@
-import React, {useContext, useEffect} from "react";
+import React, { useContext, useEffect } from "react";
 import styles from "./EditProfile.module.css";
 import Navbar from "./../navbar/Navbar";
 import { Typography } from "@material-ui/core";
 import upload from "./images/Group 2043.svg";
-import { dataCentral } from './../../context';
-import { useHistory } from 'react-router';
-
+import { dataCentral } from "./../../context";
+import { useHistory } from "react-router";
 
 const EditProfile = () => {
-  const { open, setOpen, jwt } = useContext(dataCentral)
-  const history = useHistory()
+  const { open, setOpen, jwt } = useContext(dataCentral);
+  const history = useHistory();
   useEffect(() => {
-  checkJwt()
-  }, [])
-  
+    checkJwt();
+  }, []);
+
   const checkJwt = () => {
     if (jwt) {
-      
     } else {
-      history.push('/login')
+      history.push("/login");
     }
-  }
+  };
   return (
     <>
       <Navbar />
       <div className={styles.edit_profile}>
         <div className={styles.edit_content}>
-                  <div className={styles.edit_title}>
+          <div className={styles.edit_title}>
             <Typography>Get Started</Typography>
           </div>
           <div className={styles.upload}>
@@ -42,10 +40,9 @@ const EditProfile = () => {
               <input type="date" />
             </label>
             <label htmlFor="">
-             
-                          <select name="" id="">
-                              <option value="">Add Debit/Credit Card</option>
-             </select>
+              <select name="" id="">
+                <option value="">Add Debit/Credit Card</option>
+              </select>
             </label>
             <button>SAVE</button>
           </form>
